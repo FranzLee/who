50deb1d94bcda5c7d62f01298c1f6231b6527022
Yuan <96683012+yuan0337@users.noreply.github.com>
2024-07-02 01:07:37 +0800

    Add files via upload

---
+++ employee-stock-subscription.py
@@ -0,0 +1,114 @@
+import json
+import urllib.request as req
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import re
+
+data_list = []
+companiesCode = []
+market = [0, 1, 2, 3]
+# market = [0]
+counter = 0
+document = open("who.txt", "w", encoding="utf8")
+n = 1
+def regularExpression(txt):
+    arr = re.findall("^\d{4}", txt)
+    if len(arr) == 1:
+        return True
+    else:
+        return False
+    
+def check(code):
+    result = True
+    for i in range(len(companiesCode)):
+        if(companiesCode[i] == code):
+            result = False
+    return result
+
+driver = webdriver.Chrome()
+driver.get("https://mops.twse.com.tw/mops/web/t158sb06")
+time.sleep(3)
+searchStartYear = driver.find_element("id", "startYear")
+searchStartYear.send_keys("113")
+searchEndYear = driver.find_element(By.NAME, "endYear")
+searchEndYear.send_keys("113")
+for k in range(len(market)):
+    marketSegment = Select(driver.find_elements(By.NAME, "TYPEK")[1])
+    marketSegment.select_by_index(market[k])
+    driver.execute_script("javascript:doAction();ajax1(document.form1,'table01');")
+    time.sleep(5)
+    oddElements = driver.find_elements(By.CLASS_NAME, "odd")
+    print(len(oddElements))
+    if (len(oddElements) >= 4):
+        for i in range(len(oddElements)):
+            if counter == 2:
+                if n >=10:
+                    document.write(f"\t公司名稱: {oddElements[i].text}\n")
+                    counter -= 1
+                else:
+                    document.write(f"\t公司名稱: {oddElements[i].text}\n")
+                    counter -= 1
+            elif counter == 1:
+                if n >= 10:
+                    document.write(f"\t審核日期: {oddElements[i].text}\n\n")
+                    counter -= 1
+                else:
+                    document.write(f"\t審核日期: {oddElements[i].text}\n\n")
+                    counter -= 1
+            elif regularExpression(oddElements[i].text):
+                if(check(oddElements[i].text)):
+                    if n >= 10:
+                        document.write(f"{n}:")
+                        document.write(f" 公司代碼: {oddElements[i].text}\n")
+                        companiesCode.append(oddElements[i].text)
+                        counter = 2
+                        n +=1
+                    else:
+                        document.write(f"{n}:")
+                        document.write(f"  公司代碼: {oddElements[i].text}\n")
+                        companiesCode.append(oddElements[i].text)
+                        counter = 2
+                        n +=1
+    else:
+        print(f"Warning: Expected 4 and 5 elements, but got {len(oddElements)}")
+    counter = 0
+    
+    evenElements = driver.find_elements(By.CLASS_NAME, "even")
+    print(len(evenElements))
+    if (len(evenElements) >= 4):
+        for i in range(len(evenElements)):
+            if counter == 2:
+                if n >= 10:
+                    document.write(f"\t公司名稱: {evenElements[i].text}\n")
+                    counter -= 1
+                else:
+                    document.write(f"\t公司名稱: {evenElements[i].text}\n")
+                    counter -= 1
+            elif counter == 1:
+                if n >= 10:
+                    document.write(f"\t審核日期: {evenElements[i].text}\n\n")
+                    counter -= 1
+                else:
+                    document.write(f"\t審核日期: {evenElements[i].text}\n\n")
+                    counter -= 1
+            elif regularExpression(evenElements[i].text):
+                if check(evenElements[i].text):
+                    if n >= 10:
+                        document.write(f"{n}:")
+                        document.write(f" 公司代碼: {evenElements[i].text}\n")
+                        companiesCode.append(evenElements[i].text)
+                        counter = 2
+                        n+=1
+                    else:
+                        document.write(f"{n}:")
+                        document.write(f"  公司代碼: {evenElements[i].text}\n")
+                        companiesCode.append(evenElements[i].text)
+                        counter = 2
+                        n+=1
+        counter = 0
+    else:
+        print(f"Warning: Expected 4 and 5 elements, but got {len(evenElements)}")
